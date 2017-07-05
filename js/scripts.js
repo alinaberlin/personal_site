342d25db224a8ed1c87e7c676cd9b68586457db5
Alina Ghetler <alinaghetler@Alinas-MacBook-Air.local>
2017-07-05 13:51:47 +0200

    update the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){ 
-   for(var i = 0; i < work.length; ++i ) {
-     $("#work").append("\
-    <div class='col-sm-6 col-md-3'>\
-      <img class='img-responsive' src='" + work[i] + "'>\
-    </div>\
-  ");
-  var images = $("#work img");
-   if(i%2 === 0){
-      $(images[i]).css("border", "2px solid DodgerBlue");
-    } else {
-      $(images[i]).css("border", "2px solid salmon");
-    };
-  }
+  for(var i = 0; i < work.length; ++i ) {
+    $("#work").append("\
+   <div class='col-sm-6 col-md-3'>\
+     <a href='#' class='work-img'>\
+     <img class='img-responsive' src='" + work[i].pic + "'>\
+     <span class='info'><p class='proj-title'>Title:</p> "+ work[i].title + "</span>\
+     </a>\
+   </div>\
+ ");
+ var images = $("#work img");
+  if(i%2 === 0){
+     $(images[i]).css("border", "2px solid DodgerBlue");
+   } else {
+     $(images[i]).css("border", "2px solid salmon");
+   };
+ }
+$(".work-img").mouseenter(function(){
+   $(".info", this).show();
+   }).mouseleave(function(){
+   $(".info", this).hide();
+   });
 });
--- js/work.js
+++ js/work.js
@@ -1,1 +1,14 @@
-var work = ["img/kitty.jpg", "img/kitty.jpg", "img/kitty.jpg", "img/kitty.jpg", "img/kitty.jpg"];
+var work = [
+  { 
+    title: "First Project", pic : "img/kitty.jpg"
+  },
+   {
+     title: "Second Project", pic : "img/kitty.jpg"
+    }, 
+   {
+     title: "Third Project", pic : "img/kitty.jpg"
+    }, 
+   { 
+     title: "Fourth project", pic : "img/kitty.jpg"
+    }, 
+   ];
